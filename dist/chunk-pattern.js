e6e2997df6f27a6c6ee7f5a8d446f9e3ed47fcda
srph <seraphipod@gmail.com>
2018-08-21 18:25:23 +0800

    Publish v1.0.2

--- dist/chunk-pattern.js
+++ dist/chunk-pattern.js
@@ -1,2 +1,2 @@
-"use strict";module.exports=function(t,n){var u=[],e=0,r=0;return t.forEach(function(t,h){0===r&&(u.push([]),h>0&&(e=(e+1)%n.length),r=n[e]),u[u.length-1].push(t),r--}),u};
+"use strict";module.exports=function(t,n){var u=[],e=0,r=n[0];return t.forEach(function(t){0===r&&(u.push([]),r=n[e=(e+1)%n.length]),u[u.length-1].push(t),r--}),u};
 //# sourceMappingURL=chunk-pattern.js.map
--- dist/chunk-pattern.mjs
+++ dist/chunk-pattern.mjs
@@ -1,2 +1,2 @@
-export default function(n,t){var u=[],e=0,h=0;return n.forEach(function(n,r){0===h&&(u.push([]),r>0&&(e=(e+1)%t.length),h=t[e]),u[u.length-1].push(n),h--}),u};
+export default function(n,t){var u=[],e=0,h=t[0];return n.forEach(function(n){0===h&&(u.push([]),h=t[e=(e+1)%t.length]),u[u.length-1].push(n),h--}),u};
 //# sourceMappingURL=chunk-pattern.mjs.map
